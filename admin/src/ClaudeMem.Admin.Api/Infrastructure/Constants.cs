ba220c4c6c0332ae68785cec579ae4a89ec69aec
Xavier <xavier@vvc.dev>
2026-06-24 15:43:30 +0200

    remove unused class

--- admin/src/ClaudeMem.Admin.Api/Infrastructure/Constants.cs
+++ admin/src/ClaudeMem.Admin.Api/Infrastructure/Constants.cs
@@ -4,10 +4,6 @@ namespace ClaudeMem.Admin.Api.Infrastructure
 {
     internal static class Constants
     {
-        public static class AppSettingKeys
-        {
-
-        }
         public static class StatusCodeConventions
         {
             public const int ValidationFailedStatusCode = StatusCodes.Status422UnprocessableEntity;
